ffed08b53e352a253fe059503c41b09f5384e958
Dzmitry_Trafimau <dzmitry.k.trafimau@gmail.com>
2019-07-18 15:25:42 +0300

    pretty

---
+++ src/components/CounterButton2.js
@@ -0,0 +1,35 @@
+import React, { PureComponent } from "react";
+
+class CounterButton2 extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+  shouldComponentUpdate(
+    nextProps,
+
+    nextState
+  ) {
+    if (this.state.count !== nextState.count) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  updateCount = () => {
+    this.setState(state => {
+      return { count: state.count + 1 };
+    });
+  };
+  render() {
+    return (
+      <button color={this.props.color} onClick={this.updateCount}>
+        Count:{this.state.count}
+      </button>
+    );
+  }
+}
+
+export default CounterButton2;
