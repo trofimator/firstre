b52dd387608a4a9ca558c9142367e50dba349d87
Dzmitry_Trafimau <dzmitry.k.trafimau@gmail.com>
2019-07-18 12:21:04 +0300

    Updates

---
+++ precache-manifest.246abe552255e19a1d5a46c6dc4d0a36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/trofimator.github.io/firstre/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "96e6f966b8014547a0a4",
+    "url": "/trofimator.github.io/firstre/static/js/runtime~main.96e6f966.js"
+  },
+  {
+    "revision": "cb32786862ab00f2c339",
+    "url": "/trofimator.github.io/firstre/static/js/main.cb327868.chunk.js"
+  },
+  {
+    "revision": "a6fdf1c938560a112bd7",
+    "url": "/trofimator.github.io/firstre/static/js/1.a6fdf1c9.chunk.js"
+  },
+  {
+    "revision": "cb32786862ab00f2c339",
+    "url": "/trofimator.github.io/firstre/static/css/main.536c8f78.chunk.css"
+  },
+  {
+    "revision": "a6fdf1c938560a112bd7",
+    "url": "/trofimator.github.io/firstre/static/css/1.062374d5.chunk.css"
+  },
+  {
+    "revision": "b7cf7a867a46f9b279a338f17800caba",
+    "url": "/trofimator.github.io/firstre/index.html"
+  }
+];
